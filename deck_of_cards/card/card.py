f3525c556d297be971a8c34d899c2eaa147772db
l7g9 <lukewgregory@gmail.com>
2023-05-06 22:22:04 +0100

    add docstring and type hints

--- deck_of_cards/card/card.py
+++ deck_of_cards/card/card.py
@@ -6,20 +6,28 @@ import functools
 
 @functools.total_ordering
 class Card:
-    def __init__(self, value, suit, face_up=False):
+    """Class to represent a playing card from a standard deck of cards.
+
+    Attributes:
+        value: The Value of the card Ace, Two, Three ... King
+        suit: The Suit of the card Diamonds, Spades etc.
+        face_up: A boolean set to True if the card is face up.
+    """
+    def __init__(self, value: Value, suit: Suit, face_up: bool = False):
         self.value = value
         self.suit = suit
         self.face_up = face_up
 
-    def _is_valid_operand(self, other):
+    def _is_valid_operand(self, other) -> bool:
         return (hasattr(other, "value") and hasattr(other, "suit"))
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if not self._is_valid_operand(other):
             return NotImplemented
+
         return ((self.value == other.value) and (self.suit == other.suit))
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         if not self._is_valid_operand(other):
             return NotImplemented
 
@@ -28,10 +36,20 @@ class Card:
         else:
             return int(self.value) < int(other.value)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.description(False)
 
-    def description(self, ignore_face_up):
+    def description(self, ignore_face_up: bool) -> str:
+        """Returns a text description of the card.
+
+        Args:
+            ignore_face_up: A boolean which when set to True makes this
+            method print the full desciption of the card even if it is
+            face_up is False.
+
+        Returns:
+            A string descibing the card.
+        """
         if ignore_face_up or self.face_up:
             if self.value == Value.Jocker:
                 return ("%s %s" % (self.suit, self.value))
@@ -41,19 +59,5 @@ class Card:
             return "Facedown card"
 
     def flip(self):
+        """Flips the card over from face up to face down and vice versa."""
         self.face_up = not self.face_up
-
-
-if __name__ == '__main__':
-    card1 = Card(Value.Ace, Suit.Spades)
-    print(card1)
-    card1.flip()
-    print(card1)
-
-    card2 = Card(Value.Ace, Suit.Hearts)
-    print(card1 < card2)
-    print(card1 > card2)
-
-    card3 = Card(Value.Jocker, Suit.Black)
-    card3.flip()
-    print(card3)
