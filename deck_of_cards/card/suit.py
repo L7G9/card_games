85403959098ecc105dd92d3733515bbc5e2120bb
l7g9 <lukewgregory@gmail.com>
2023-05-06 21:51:18 +0100

    add docstring and type hinting

--- deck_of_cards/card/suit.py
+++ deck_of_cards/card/suit.py
@@ -1,12 +1,22 @@
 from enum import Enum
 
+
 class Suit(Enum):
-    def __str__(self):
+    """Enumeration class for the suit of a playing card.
+
+    The name attribute represents the name of the suit Clubs, Diamonds, Hearts
+    and Spades.  Black and Red are used for the Suit of the Jokers.
+    The value attribute represents the value comparitive value of each suit
+    eg. Spades are worth more than Hearts.
+    """
+    def __str__(self) -> str:
         return self.name
 
-    def __int__(self):
+    def __int__(self) -> str:
         return self.value
 
+    Black = 0
+    Red = 0
     Clubs = 1
     Diamonds = 2
     Hearts = 3
