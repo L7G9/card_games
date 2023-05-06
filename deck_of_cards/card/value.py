b4d865c0ceef13cfc01dd50f99d7751b71159c9d
l7g9 <lukewgregory@gmail.com>
2023-05-06 21:52:06 +0100

    add dockstring and type hinting

--- deck_of_cards/card/value.py
+++ deck_of_cards/card/value.py
@@ -1,12 +1,20 @@
 from enum import Enum
 
+
 class Value(Enum):
-    def __str__(self):
+    """Enumeration class for the value of a playing card.
+
+    The name attribute represents the name of the card from Ace to King and
+    Jokers.
+    The value attribute represents the value of the card in the game.
+    """
+    def __str__(self) -> str:
         return self.name
 
-    def __int__(self):
+    def __int__(self) -> int:
         return self.value
 
+    Jocker = 0
     Ace = 1
     Two = 2
     Three = 3
