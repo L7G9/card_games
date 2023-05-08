540e3df9ef7271dc48d08bc425edd5842df8d9e2
l7g9 <lukewgregory@gmail.com>
2023-05-08 21:11:56 +0100

    add id

--- model/player.py
+++ model/player.py
@@ -5,9 +5,11 @@ class Player:
     """Class to represent a player in a card game.
 
     Attributes:
+        id: An int to uniquely identify a player.
         name: A string describing this player.
         hand: A CardGroup for the Cards this player is holding.
     """
-    def __init__(self, name: str, hand: CardGroup):
+    def __init__(self, id: int, name: str, hand: CardGroup):
+        self.id = id
         self.name = name
         self.hand = hand
