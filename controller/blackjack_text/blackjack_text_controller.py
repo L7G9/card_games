50c39066070a590a29f81c067ebb082b84a5e726
l7g9 <lukewgregory@gmail.com>
2023-05-15 16:06:14 +0100

    re-work main loop and add player creation to controller

--- controller/blackjack_text/blackjack_text_controller.py
+++ controller/blackjack_text/blackjack_text_controller.py
@@ -1,27 +1,79 @@
 from time import sleep
+from os import system, name
 
-from model.card_game.card import Card
 from model.blackjack.game import Game
 from model.blackjack.game_status import GameStatus
 from model.blackjack.player import Player
+from model.blackjack.action_selector import ActionSelector
 from model.blackjack.player_status import PlayerStatus
-
-from view.text_view import text_view as view
 
 
 class BlackjackTextController:
     """Controller for BlackJackGame and TextView."""
+
+    names = [
+        "Adam",
+        "Betty",
+        "Chris",
+        "Denise",
+        "Ethan",
+        "Francesca",
+        "Gregory",
+        "Harriet",
+        "Jake"
+    ]
+
     def __init__(self):
         self.game = Game("Blackjack")
-        self.view = view.TextView()
 
-    def play_game(self) -> list[Player]:
-        # setup game
+    def run(self):
+        self.clear()
+        print("Welcome to our game of Blackjack.")
+        sleep(1)
+
+        self.setup()
+
+        continue_playing = True
+        while continue_playing:
+            self.play_game()
+
+            winners = self.resolve_game()
+
+            play_again = self.view.read("Play again? (y or n): ")
+            continue_playing = play_again == 'y'
+            if continue_playing:
+                self.reset_game(winners)
+
+    def setup(self):
+        player_name = input("Enter your name: ")
+        other_count = int(input("Enter number of other players (3 to 9): "))
+
+        player = Player(0, player_name)
+        self.game.players.append(player)
+        for i in range(0, other_count):
+            player = Player(i+1, self.names[i], ActionSelector())
+            self.game.players.append(player)
+
+        print("Our players are...")
+        for player in self.game.players:
+            print(player.name)
+
+        sleep(2)
+        self.clear()
+
+    def play_game(self):
+        print("Dealing.")
         self.game.deal()
+        sleep(1)
+        self.clear()
 
-        # play game
-        while self.game.status != GameStatus.RESOLVING:
+        while self.game.next_player() != GameStatus.RESOLVING:
             active_player = self.game.players[self.game.active_player_index]
+            self.player_turn(active_player)
+
+    def player_turn(self, active_player):
+        while self.game.status != GameStatus.NEXT_PLAYER:
+            print("It is %s's turn." % (active_player.name))
             self.game.send_actions(active_player)
 
             stick = False
@@ -31,72 +83,74 @@ class BlackjackTextController:
                     self.game.game_stats
                 )
             else:
-                self.view.write(active_player.hand.description())
+                print(active_player.hand.description())
                 for card in active_player.hand.cards:
-                    self.view.write(card.description(True))
-                chosen_action = self.view.read("Stick or Twist? (s or t): ")
+                    print(card.description(True))
+                chosen_action = input("Stick or Twist? (s or t): ")
                 if chosen_action == 's':
                     stick = True
                 elif chosen_action == 'f':
                     stick = False
 
             if stick:
-                self.view.write("%s sticks." % (active_player.name))
+                print("%s sticks." % (active_player.name))
                 self.game.resolve_stick_action(active_player)
             else:
-                self.view.write("%s twists." % (active_player.name))
+                print("%s twists." % (active_player.name))
                 player_status, card = self.game.resolve_twist_action(
                     active_player
                 )
                 if active_player.action_selector is None:
-                    self.view.write("And draws %s." % (card.description(True)))
+                    print("And draws %s." % (card.description(True)))
                 if active_player.status == PlayerStatus.BUST:
-                    self.view.write("%s goes bust." % (active_player.name))
+                    print("%s goes bust." % (active_player.name))
 
             sleep(2)
+            self.clear()
 
-        # resolve game
+    def resolve_game(self):
+        print("Results.")
+
         games_status, winners = self.game.resolve_game()
 
-        self.view.write("")
-        self.view.write("Game complete.")
-        self.view.write("")
         for player in self.game.players:
-            self.view.write("%s has a total of %d with..." % (
-                player.name,
-                player.best_total))
+            print("%s reveals their cards..." % (player.name))
             for card in player.hand.cards:
-                self.view.write(card.description(False))
-            self.view.write("")
-            sleep(2)
+                print(card.description(False))
+            if player.status == PlayerStatus.BUST:
+                print("Went bust.")
+            else:
+                print("Has a total of %d." % (player.best_total))
+            sleep(1)
 
-        self.view.write("The winner(s) of this round is...")
+        if len(winners) == 0:
+            print("No winners this round.")
+        elif len(winners) == 1:
+            print("The winner of this round is...")
+            sleep(1)
+            print(winners[0].name)
+        else:
+            print("The winners of this round are...")
+            for player in winners:
+                sleep(1)
+                print(player.name)
+
         sleep(2)
-        for player in winners:
-            self.view.write(player.name)
-        self.view.write("")
 
         return winners
 
-    def run(self):
-        self.view.write("Welcome to our game of Blackjack.")
-        self.setup_user_player()
+    def reset_game(self, winners):
+        print("Resetting game.")
+        self.game.reset_game(winners)
+        print("Player order now is...")
+        for player in self.game.players:
+            self.view.write(player.name)
 
-        continue_playing = True
-        while continue_playing is True:
+        sleep(2)
+        self.clear()
 
-            winners = self.play_game()
-
-            # reset game
-            play_again = self.view.read("Play again? (y or n): ")
-            continue_playing = play_again == 'y'
-            if continue_playing:
-                self.game.reset_game(winners)
-                self.view.write("Player order now is...")
-                for player in self.game.players:
-                    self.view.write(player.name)
-
-    def setup_user_player(self):
-        name = self.view.read("Enter name: ")
-        player = Player(3, name)
-        self.game.players.append(player)
+    def clear(self):
+        if name == 'nt':
+            _ = system('cls')
+        else:
+            _ = system('clear')
