4348f1f06439c6a2ae89f8d246b89081302aea5e
l7g9 <lukewgregory@gmail.com>
2023-05-19 14:37:07 +0100

    add test to update so methods does nothing when all players have had a go

--- model/blackjack/game_stats.py
+++ model/blackjack/game_stats.py
@@ -17,12 +17,15 @@ class GameStats:
                 self.bust_count))
 
     def update(self, player_status: PlayerStatus):
+        if (self.unfinished_count == 0):
+            return
+
         if player_status is PlayerStatus.STICK:
             self.sticking_count += 1
-            self.unfinished_count -= 1
         elif player_status is PlayerStatus.BUST:
             self.bust_count += 1
-            self.unfinished_count -= 1
+
+        self.unfinished_count -= 1
 
     def reset(self):
         self.unfinished_count = self.player_count
