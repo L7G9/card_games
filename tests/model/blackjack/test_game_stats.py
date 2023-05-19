b653a9bb8b0bcb61d7736027d9983676a02ee65e
l7g9 <lukewgregory@gmail.com>
2023-05-19 14:38:06 +0100

    add unit tests for GameStats class

---
+++ tests/model/blackjack/test_game_stats.py
@@ -0,0 +1,22 @@
+import pytest
+
+from model.blackjack.game_stats import GameStats
+from model.blackjack.player_status import PlayerStatus
+
+@pytest.fixture(scope="function")
+def game_stats():
+    return GameStats(4)
+
+
+class TestGameStats:
+    # update game stats when a player sticks
+    def test_update_stick(self, game_stats):
+        game_stats.update(PlayerStatus.STICK)
+        assert game_stats.unfinished_count == 3
+        assert game_stats.sticking_count == 1
+
+    # update game stats when a player goes bust
+    def test_update_bust(self, game_stats):
+        game_stats.update(PlayerStatus.BUST)
+        assert game_stats.unfinished_count == 3
+        assert game_stats.bust_count == 1
