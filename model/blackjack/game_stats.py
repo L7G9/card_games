0dcceb16634bd608fec8ff3debfff9cf56b0ee91
l7g9 <lukewgregory@gmail.com>
2023-05-20 22:00:12 +0100

    add docstring

--- model/blackjack/game_stats.py
+++ model/blackjack/game_stats.py
@@ -2,8 +2,26 @@ from model.blackjack.player_status import PlayerStatus
 
 
 class GameStats:
-    """Stats for all the other player states in the game."""
+    """Stats for all the stats of all player states in the game.
+
+    Updated by the a Game instance and passed to ActionSelector to help
+    decide if a plyer should stick or twist.
+
+    Attributes:
+        player_count: An integer equal to the number of players in the game.
+        unfinished_count: An integer equal to the number of player yet to
+          start or finish their turn.
+        sticking_count: An integer equal to the number of players who have
+          chosen to stick.
+        bust_count: An integer equal to the number of players who have gone
+          bust.
+    """
     def __init__(self, player_count: int):
+        """Initializes instance.
+
+        Args:
+            player_count: An integer equal to the number of players.
+        """
         self.player_count = player_count
         self.unfinished_count = self.player_count
         self.sticking_count = 0
@@ -14,9 +32,19 @@ class GameStats:
                 self.player_count,
                 self.unfinished_count,
                 self.sticking_count,
-                self.bust_count))
+                self.sticking_count))
 
     def update(self, player_status: PlayerStatus):
+        """Updates the stats when a players' status changes.
+
+        Should be called when a player sticks or goes bust to increase
+        sticking_count or sticking_count by 1, and reduce unfinished_count
+        by 1.
+
+        Args:
+            player_status: PlayerStatus enum member of the player who chose to
+              stick or wen bust.
+        """
         if (self.unfinished_count == 0):
             return
 
