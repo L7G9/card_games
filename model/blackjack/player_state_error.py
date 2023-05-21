2f6c2f7b6920f0091996622c1a1f215df1d50d44
l7g9 <lukewgregory@gmail.com>
2023-05-21 17:19:18 +0100

    add PlayerStateError

---
+++ model/blackjack/player_state_error.py
@@ -0,0 +1,22 @@
+from model.blackjack.player_state import PlayerState
+
+
+class PlayerStateError(Exception):
+    """Player State Error.
+
+    Occur when the order which a method in Player is called is incompatible
+    with it's state.
+    For example when stick is called and the player state is BUST.
+    """
+    def __init__(
+        self,
+        current_state: PlayerState,
+        expected_states: list[PlayerState]
+    ):
+        self.current_state = current_state
+        self.expected_state = expected_states
+
+    def __str__(self):
+        return ("Current State=%s Expected States=%s" % (
+            self.current_state,
+            self.expected_state))
