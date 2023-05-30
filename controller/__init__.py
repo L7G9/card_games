90a785d4e6d7b73cce7134aa9e81a75909eb8bc0
l7g9 <lukewgregory@gmail.com>
2023-05-30 08:55:56 +0100

    add docstring for packages

---
+++ controller/__init__.py
@@ -0,0 +1,5 @@
+"""Controllers for twenty_one_bust model and various views.
+
+Sub-modules:
+    twenty_one_bust
+"""
---
+++ controller/twenty_one_bust/__init__.py
@@ -0,0 +1,5 @@
+"""Controller for twenty one bust model and text view.  
+
+Modules:
+    console_controller
+"""
