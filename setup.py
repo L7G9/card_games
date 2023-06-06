2761c6f1e3f13ef5b5c66e851299edb526f303cc
l7g9 <lukewgregory@gmail.com>
2023-06-06 22:52:40 +0100

    build: added setup.py for use of version number

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+__version__ = "0.0.0"
+
+setup(
+    name="card-games",
+    version=__version__,
+    author="Luke Gregory",
+    author_email="lukewgregory@gmail.com",
+    url="https://github.com/L7G9/card_games"
+)
