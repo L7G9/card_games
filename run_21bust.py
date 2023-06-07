f64135af38327d6ed5da64e7eaab8275cc5e1104
l7g9 <lukewgregory@gmail.com>
2023-06-07 09:30:57 +0100

    fix(run_21bust.py): update docstring

--- run_21bust.py
+++ run_21bust.py
@@ -1,4 +1,4 @@
-"""Runner for text version of Twenty One Bust."""
+"""Runner for text version of card game Twenty One Bust."""
 from controller.twenty_one_bust import console_controller as controller
 
 if __name__ == "__main__":
